e5de14fbae726a395c286209ecbb9144dea9246c
Jean-Frederic Clere <jfclere@gmail.com>
2014-11-09 22:42:59 +0100

    Arrange the proxy tests.

--- scripts/runurlproxytests.sh
+++ scripts/runurlproxytests.sh
@@ -15,44 +15,36 @@ HTTPDPORT=${6:-8089}
 HTTPDSPORT=${7:-8099}
 HTTPD_ONLY=false
 SKIP_HTTP_TESTS=true
+REPORT_FILE="results_httpd.txt"
 
 SCRIPT_DIR=`dirname "${0}"`
 
-function stop_vmstat {
-  if [ -n "${VMSTAT_PID}" ] ; then
-    echo 'Stopping vmstat'
+export REPORT_DIR
+export REPORT_FILE
 
-    kill -15 ${VMSTAT_PID}
-
-    unset VMSTAT_PID
-  fi
-}
 
 function quit {
   echo
-  stop_vmstat
-
   exit
 }
 
 trap "quit" INT TERM EXIT
 
-ssh ${HOST} vmstat -n 5 > "${REPORT_DIR}/vmstat.log" &
-VMSTAT_PID=$!
-
 if [ ! "${SKIP_HTTP_TESTS}" ] ; then
   # httpd
+  REPORT_FILE=results_httpd
   echo "Running test on http://${HOST}:${HTTPDPORT}/"
   "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 http://${HOST}:${HTTPDPORT}/ >/dev/null
   sleep ${SLEEP_TIME}
-  "${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:${HTTPDPORT}/ | tee "${REPORT_DIR}/results_httpd.txt" 2>&1
+  "${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:${HTTPDPORT}/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 fi
 if [ ! "${SKIP_HTTPS_TESTS}" ] ; then
   # httpd
+  REPORT_FILE=results_httpd_ssl
   echo "Running test on https://${HOST}:${HTTPDPORT}/"
   "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 https://${HOST}:${HTTPDSPORT}/ >/dev/null
   sleep ${SLEEP_TIME}
-  "${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:${HTTPDSPORT}/ | tee "${REPORT_DIR}/results_httpd_ssl.txt" 2>&1
+  "${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:${HTTPDSPORT}/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 fi
 if ${HTTPD_ONLY}; then
   echo "Done httpd/httpds only proxy"
@@ -61,43 +53,51 @@ if ${HTTPD_ONLY}; then
 fi
 
 # WildFly AJP
+REPORT_FILE=results_widfly_ajp
 "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 http://${HOST}:8180/tcaj/ >/dev/null
 sleep ${SLEEP_TIME}
-"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:8180/tcaj/ | tee "${REPORT_DIR}/results_widfly_ajp.txt" 2>&1
+"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:8180/tcaj/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 
 # SSL WildFly AJP
+REPORT_FILE=results_ssl_widfly_ajp
 "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 https://${HOST}:8543/tcaj/ >/dev/null
 sleep ${SLEEP_TIME}
-"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:8180/tcaj/ | tee "${REPORT_DIR}/results_ssl_widfly_ajp.txt" 2>&1
+"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:8180/tcaj/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 
 # Proxy AJP
+REPORT_FILE=results_proxy_ajp
 "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 http://${HOST}:${HTTPDPORT}/tcaj/ >/dev/null
 sleep ${SLEEP_TIME}
-"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:${HTTPDPORT}/tcaj/ | tee "${REPORT_DIR}/results_proxy_ajp.txt" 2>&1
+"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:${HTTPDPORT}/tcaj/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 
 # Proxy HTTP
+REPORT_FILE=results_proxy_http
 "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 http://${HOST}:${HTTPDPORT}/tchp/ >/dev/null
 sleep ${SLEEP_TIME}
-"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:${HTTPDPORT}/tchp/ | tee "${REPORT_DIR}/results_proxy_http.txt" 2>&1
+"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:${HTTPDPORT}/tchp/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 
 # Mod_jk
+REPORT_FILE=results_mod_jk
 "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 http://${HOST}:${HTTPDPORT}/jkaj/ >/dev/null
 sleep ${SLEEP_TIME}
-"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:${HTTPDPORT}/jkaj/ | tee "${REPORT_DIR}/results_mod_jk.txt" 2>&1
+"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} http://${HOST}:${HTTPDPORT}/jkaj/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 
 # Proxy AJP SSL tests.
+REPORT_FILE=results_ssl_proxy_ajp
 "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 https://${HOST}:${HTTPDSPORT}/tcaj/ >/dev/null
 sleep ${SLEEP_TIME}
-"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:${HTTPDSPORT}/tcaj/ | tee "${REPORT_DIR}/results_ssl_proxy_ajp.txt" 2>&1
+"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:${HTTPDSPORT}/tcaj/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 
 # Proxy HTTP
+REPORT_FILE=results_ssl_proxy_http
 "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 https://${HOST}:${HTTPDSPORT}/tchp/ >/dev/null
 sleep ${SLEEP_TIME}
-"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:${HTTPDSPORT}/tchp/ | tee "${REPORT_DIR}/results_ssl_proxy_http.txt" 2>&1
+"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:${HTTPDSPORT}/tchp/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 
 # Mod_jk
+REPORT_FILE=results_ssl_mod_jk
 "${SCRIPT_DIR}/runfiletests.sh" 1 1 0 https://${HOST}:${HTTPDSPORT}/jkaj/ >/dev/null
 sleep ${SLEEP_TIME}
-"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:${HTTPDSPORT}/jkaj/ | tee "${REPORT_DIR}/results_ssl_mod_jk.txt" 2>&1
+"${SCRIPT_DIR}/runfiletests.sh" ${REQUESTS} ${CONCURRENCY} ${TIME_LIMIT} https://${HOST}:${HTTPDSPORT}/jkaj/ | tee "${REPORT_DIR}/${REPORT_FILE}.txt" 2>&1
 
 quit
